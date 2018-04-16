334793310b400b7a2c8b54373f6d33c71381761b
jaredianmills <jaredianmills@gmail.com>
2018-04-16 16:06:41 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,8 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
 function addElementToBeginningOfArray(array, element){
-  [element, ...array];
+  var newArray = array;
+  [element, ...newArray]
   return array
 }
 
