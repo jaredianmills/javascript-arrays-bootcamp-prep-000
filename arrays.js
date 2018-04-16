676045a0bd29ca72a604cec15d2e0862bad20516
jaredianmills <jaredianmills@gmail.com>
2018-04-16 16:30:27 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -25,3 +25,5 @@ function destructivelyAddElementToEndOfArray(array, element) {
 function accessElementInArray(array, index) {
   return array[index];
 }
+
+function destructivelyRemoveElementFromBeginningOfArray(array, element)
